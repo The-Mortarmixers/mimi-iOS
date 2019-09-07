2424966bbf084cf8ae72b033eb7c41cd367d3469
Josef Büttgen <josefbuettgen@gmail.com>
2019-09-07 20:58:45 +0200

    Start storyboards

---
+++ mimi. iOS/TileARViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TileARViewController.swift
+//  mimi. iOS
+//
+//  Created by Josef Büttgen on 07.09.19.
+//  Copyright © 2019 mimi. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class TileARViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
