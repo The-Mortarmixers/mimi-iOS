22d600e317ab433eb1c1b19a7d9538a590d7cbed
Josef Büttgen <josefbuettgen@gmail.com>
2019-09-07 20:03:41 +0200

    Initial commit

---
+++ mimi. iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mimi. iOS
+//
+//  Created by Josef Büttgen on 07.09.19.
+//  Copyright © 2019 mimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mimi. iOSTests/mimi__iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  mimi__iOSTests.swift
+//  mimi. iOSTests
+//
+//  Created by Josef Büttgen on 07.09.19.
+//  Copyright © 2019 mimi. All rights reserved.
+//
+
+import XCTest
+@testable import mimi__iOS
+
+class mimi__iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
